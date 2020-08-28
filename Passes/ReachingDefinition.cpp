839f9f266370755c7ecc899d02d414a3918789d8
Chibin Zhang <chibinzhang@hotmail.com>
2020-08-28 16:48:28 +0800

    Add blank ReachingDefinition pass

---
+++ Passes/ReachingDefinition.cpp
@@ -0,0 +1,37 @@
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Type.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+
+using namespace llvm;
+
+static auto PASS_NAME = "ReachingDefinitionPass";
+static auto PASS_VERSION = "v0.1";
+static auto ARGUMENT_NAME = "reaching";
+
+namespace {
+struct ReachingDefinitionPass : public PassInfoMixin<ReachingDefinitionPass> {
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &) {
+
+
+    return PreservedAnalyses::none();
+  }
+};
+} // namespace
+
+extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK
+llvmGetPassPluginInfo() {
+  return {LLVM_PLUGIN_API_VERSION, PASS_NAME, PASS_VERSION,
+          [](PassBuilder &PB) {
+            PB.registerPipelineParsingCallback(
+                [](StringRef Name, FunctionPassManager &FPM,
+                   ArrayRef<PassBuilder::PipelineElement>) {
+                  if (Name == ARGUMENT_NAME) {
+                    FPM.addPass(ReachingDefinitionPass());
+                    return true;
+                  } else {
+                    return false;
+                  }
+                });
+          }};
+}
