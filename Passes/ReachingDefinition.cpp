31e614283d873816f973e1409ebad0b12cc0adec
Chibin Zhang <chibinzhang@hotmail.com>
2020-08-28 23:08:50 +0800

    Implement instruction indexing

--- Passes/ReachingDefinition.cpp
+++ Passes/ReachingDefinition.cpp
@@ -1,3 +1,6 @@
+#include <set>
+
+#include "Bimap.h"
 #include "llvm/IR/Instruction.h"
 #include "llvm/IR/Type.h"
 #include "llvm/Passes/PassBuilder.h"
@@ -9,10 +12,29 @@ static auto PASS_NAME = "ReachingDefinitionPass";
 static auto PASS_VERSION = "v0.1";
 static auto ARGUMENT_NAME = "reaching";
 
+// Stuffs to implement
+// Transfer functions - operate on instructions
+// Join operator - operate on basic blocks
+// Instruction indexing
+
+Bimap<Value *, unsigned> InstrIndexBimap;
+using GenSet = std::set<unsigned>;
+using KillSet = std::set<unsigned>;
+
+auto indexInstructions(Function &F) {
+  auto count = 0;
+
+  for (auto &BB: F) {
+    for (auto &I: BB) {
+      count += 1;
+      InstrIndexBimap.insert(&I, count);
+    }
+  }
+}
+
 namespace {
 struct ReachingDefinitionPass : public PassInfoMixin<ReachingDefinitionPass> {
   PreservedAnalyses run(Function &F, FunctionAnalysisManager &) {
-
 
     return PreservedAnalyses::none();
   }
